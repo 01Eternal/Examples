40a872dd9059ac1afbc1f5252e1074a674d6d88f
Eternal <109238149+01Eternal@users.noreply.github.com>
2024-05-08 18:41:53 -0300

    Atualizar o Wings Panic

--- Player Example/Unlimited Potion Buffs.js
+++ Player Example/Unlimited Potion Buffs.js
@@ -1,8 +1,10 @@
 // author 01Eternal
+
 const Player = new NativeClass('Terraria', 'Player');
 const ItemID = new NativeClass('Terraria.ID', 'BuffId');
 
 Player.WingMovement.hook((original, self) => {
 original(self);
+
 self.AddBuff(BuffID.Panic, 60, true, false);
 });
