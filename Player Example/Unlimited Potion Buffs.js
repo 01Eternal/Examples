287141503b262e90b9608b904e6f5770a855ba7c
Eternal <109238149+01Eternal@users.noreply.github.com>
2024-05-08 18:41:16 -0300

    Atualizar o Unlimited Potion Buffs.js

--- Player Example/Unlimited Potion Buffs.js
+++ Player Example/Unlimited Potion Buffs.js
@@ -3,7 +3,6 @@ const Player = new NativeClass('Terraria', 'Player');
 const ItemID = new NativeClass('Terraria.ID', 'BuffId');
 
 Player.WingMovement.hook((original, self) => {
-original(self)
-
-}
-
+original(self);
+self.AddBuff(BuffID.Panic, 60, true, false);
+});
